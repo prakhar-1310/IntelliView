e234a0ee30c934fc831852b31fc99d3c2944f72c
Prakhar <prakharshahi9935@gmail.com>
2025-12-31 02:30:27 +0530

    deployment config added

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,14 +1,25 @@
 import express from "express";
-import {ENV} from "./lib/env.js"
+import {ENV} from "./lib/env.js";
+import path from "path";
 
 const app = express();
 
-app.get("/", (req,res)=>{
+const __dirname = path.resolve();
+
+app.get("/msg", (req,res)=>{
     res.status(200).json({
         msg: "success from API123",
         msg2 : "API"
     })
 })
+
+if(ENV.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname,"../frontend/dist")))  // making dist as static assets
+
+    app.get("/{*any}", (req,res)=>{
+        res.sendFile( path.join(__dirname,"../frontend/dist/index.html"));
+    })
+}
 
 app.listen(ENV.PORT, ()=>{
     console.log(`Server is running on port ${ENV.PORT}`);
